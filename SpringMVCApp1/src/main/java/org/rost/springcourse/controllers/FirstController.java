80d56564fc8490052e13be59598320814ccb1bcd
Hnatiuk Rostyslav <hnatiukrost@gmail.com>
2022-10-01 06:50:16 +0300

    add lessons 20-21

--- SpringMVCApp1/src/main/java/org/rost/springcourse/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/org/rost/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.rost.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,40 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam (value = "surname", required = false) String surname){
-    System.out.println("Hello" + name + " " + surname);
+                            @RequestParam (value = "surname", required = false) String surname,
+                            Model model){
+    //System.out.println("Hello" + name + " " + surname);
+    model.addAttribute("message", "Hello" + name + " " + surname);
+
     return "first/hello";
 }
 
-    @GetMapping("/goodbye")
-    public String goodByePage() {
-        return "first/goodbye";
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+        double result = 0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                if (a == 0 || b == 0) {
+                    System.out.println("on 0 can't division");
+                    break;
+                }
+                result = a / b;
+                break;
+        }
+        model.addAttribute("result", " " + result);
+        return "first/calculator";
     }
 
 }

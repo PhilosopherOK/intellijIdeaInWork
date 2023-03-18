a2eaf754be8c723c68e5aec57ca39b83e2b2d973
Hnatiuk Rostyslav <hnatiukrost@gmail.com>
2023-03-18 08:14:19 +0200

    add registration in security app

--- SpringRepetition/src/main/java/ua/example/SpringRepetition/config/SecurityConfig.java
+++ SpringRepetition/src/main/java/ua/example/SpringRepetition/config/SecurityConfig.java
@@ -1,22 +1,49 @@
 package ua.example.SpringRepetition.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import ua.example.SpringRepetition.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ua.example.SpringRepetition.service.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProviderImpl;
+
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProviderImpl) {
-        this.authProviderImpl = authProviderImpl;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
-        authenticationManagerBuilder.authenticationProvider(authProviderImpl);
+    protected void configure(HttpSecurity http) throws Exception{
+        //config security and autorization
+        http.formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
     }
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+//    private final AuthProviderImpl authProviderImpl;
+//
+//    @Autowired
+//    public SecurityConfig(AuthProviderImpl authProviderImpl) {
+//        this.authProviderImpl = authProviderImpl;
+//    }
+//
+//    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
+//        authenticationManagerBuilder.authenticationProvider(authProviderImpl);
+//    }
 }
--- SpringRepetition/src/main/java/ua/example/SpringRepetition/controllers/HelloController.java
+++ SpringRepetition/src/main/java/ua/example/SpringRepetition/controllers/HelloController.java
@@ -1,12 +1,23 @@
 package ua.example.SpringRepetition.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.example.SpringRepetition.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
+        return "/hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
         return "/hello";
     }
 }

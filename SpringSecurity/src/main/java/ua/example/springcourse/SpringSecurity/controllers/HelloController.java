656dc3a3454036461bd8787fc7aa9783e901c2d4
Hnatiuk Rostyslav <hnatiukrost@gmail.com>
2023-03-23 05:08:22 +0200

    add secur method @PreAuthorize what use role option

--- SpringSecurity/src/main/java/ua/example/springcourse/SpringSecurity/controllers/HelloController.java
+++ SpringSecurity/src/main/java/ua/example/springcourse/SpringSecurity/controllers/HelloController.java
@@ -1,13 +1,23 @@
 package ua.example.springcourse.SpringSecurity.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ua.example.springcourse.SpringSecurity.security.PersonDetails;
+import ua.example.springcourse.SpringSecurity.service.AdminService;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String sayHello(){
         return "/hello";
